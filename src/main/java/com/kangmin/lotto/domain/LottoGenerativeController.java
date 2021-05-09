225de08b1e767b0284ad7bb793052ff0f2109658
boring-km <kms0644804@naver.com>
2021-05-09 22:58:15 +0900

    [Feature-01] 컨트롤러 명을 좀더 구체적으로 하기
    필요한 객체만 주입받을 수 있도록 어노테이션을 수정한다.

--- src/main/java/com/kangmin/lotto/domain/LottoGenerativeController.java
+++ src/main/java/com/kangmin/lotto/domain/LottoGenerativeController.java
@@ -2,7 +2,7 @@ package com.kangmin.lotto.domain;
 
 import com.kangmin.lotto.service.CustomGenerator;
 import com.kangmin.lotto.service.RandomGenerator;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.LinkedHashMap;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
-public class LottoController {
+public class LottoGenerativeController {
 
     private final RandomGenerator randomGenerator;
 
