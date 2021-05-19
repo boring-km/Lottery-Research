dc67153dff7127acd71b04ba72776d95c40f3d72
boring-km <kms0644804@naver.com>
2021-05-20 00:00:43 +0900

    [Feature-02] 로또 회차 번호를 입력하여 콘솔에서 조회 결과를 출력

--- front-end/src/components/MainPage.vue
+++ front-end/src/components/MainPage.vue
@@ -1,10 +1,30 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <div id="root">
+    <h1>{{ msg }}</h1>
+    <form v-on:submit="onSubmitForm">
+      <input type="text" v-model="round">
+      <button>검색</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
   name: "MainPage",
+  data() {
+    return {
+      round: ''
+    }
+  },
+  methods: {
+    onSubmitForm(e) {
+      e.preventDefault();
+      console.log(this.round);
+      this.axios.get(`http://localhost:8080/lotto/search/${this.round}`)
+          .then(response => console.log(response))
+          .catch(err => console.log(err));
+    }
+  },
   props: {
     msg: String
   }
