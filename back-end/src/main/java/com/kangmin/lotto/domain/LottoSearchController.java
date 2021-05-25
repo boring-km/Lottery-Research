2e487a719dbf8d40814df1a75a9d133c217d953a
boring-km <kms0644804@naver.com>
2021-05-25 23:54:02 +0900

    [Feature-03] 회차별 평균합을 구할 수 있는 컨트롤러 추가

--- back-end/src/main/java/com/kangmin/lotto/domain/LottoSearchController.java
+++ back-end/src/main/java/com/kangmin/lotto/domain/LottoSearchController.java
@@ -1,6 +1,7 @@
 package com.kangmin.lotto.domain;
 
 import com.kangmin.lotto.service.api.LottoSearching;
+import com.kangmin.lotto.service.dao.MongoDAO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class LottoSearchController {
 
     private final LottoSearching searching;
+    private final MongoDAO mongoDAO;
 
     @GetMapping("/lotto/search/{round}")
     public LottoRecord searchRecord(@PathVariable int round) {
         return searching.getLottoByRound(round);
     }
 
+    @GetMapping("/lotto/recent")
+    public int searchRecentRound() {
+        return searching.getRecentRound();
+    }
+
+    @GetMapping("/lotto/average")
+    public int getAverageSumByRound() {
+        return mongoDAO.getAverageSumByRound();
+    }
 }
